b86f64e1aaf30c2a44db889b8f606b3a0774f31b
Sejan Dhakal <dhakalsejan124@gmail.com>
2024-09-23 13:05:36 -0500

    Very basic GUI implementation.

--- interface.py
+++ interface.py
@@ -1,5 +1,6 @@
 import os
 import datetime
+import PySimpleGUI as sg
 
 diaryfile = "diary.txt"
 
@@ -11,13 +12,9 @@ def menu():
     print("4: Exit out of diary.")
     return input("Choose one of the options: ")
 
-def write():
+def write(entry_date, entry_text):
     with open(diaryfile, 'a') as file:
-        entry_date = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
-        print(f"Date and time: {entry_date}")
-        entry = input("Write your entry: ")
-        file.write(f"\n{entry_date}\n{entry}\n{'-'*20}\n")
-        print("Your entry has been added successfully.")
+        file.write(f"\n{entry_date}\n{entry_text}\n{'-'*20}\n")
 
 def view():
     if os.path.exists(diaryfile):
@@ -38,20 +35,57 @@ def delete():
     else:
         print("There are no entries to delete.")
 
-def main():
-    while True:
-        option = menu()
-        if option == "1":
-            write()
-        elif option == "2":
-            view()
-        elif option == "3":
-            delete()
-        elif option == "4":
-            print("Exiting diary. See you again!")
-            break
-        else:
-            print("Invalid option, try again.")
+# def main():
+#     while True:
+#         option = menu()
+#         if option == "1":
+#             write()
+#         elif option == "2":
+#             view()
+#         elif option == "3":
+#             delete()
+#         elif option == "4":
+#             print("Exiting diary. See you again!")
+#             break
+#         else:
+#             print("Invalid option, try again.")
+
+layout = [  [sg.Text('Welcome to diary!')],
+            [sg.Text('What would you like to do today?')],
+            [sg.Button('Write')],
+            [sg.Button('View all entries')], 
+            [sg.Button('Delete all entries')],
+            [sg.Button('Exit')]]
+
+window = sg.Window('Diary', layout)
+entry_date = datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S")
+
+while True:
+    event, values = window.read()
+    if event == 'Write':
+        write_layout = [[sg.Text('Time to write!'), sg.Input(key='entry_text')],
+                        [sg.Text('Date and time: ' + entry_date)],
+                        [sg.Button('Save')],
+                        [sg.Button('Exit')]]
+        write_window = sg.Window('writing', write_layout)
+        while True:
+            write_event, write_values = write_window.read()
+            if write_event == 'Save':
+                write(entry_date, write_values['entry_text'])
+                sg.popup('Your entry has been saved!', no_titlebar=True)
+                break
+            elif event == 'Exit':
+                sg.popup('Exiting writing mode...', no_titlebar=True)
+                break
+    if event == 'View all entries':
+        view()
+    if event == 'Delete all entries':
+        delete()
+    if event == sg.WIN_CLOSED or event == 'Exit': 
+        sg.popup('Exiting diary. See you soon!.', no_titlebar=True)
+        break
+
+window.close()
 
 if __name__ == "__main__":
     main()
